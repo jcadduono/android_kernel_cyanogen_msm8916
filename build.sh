e63b8fe00020ec5c206ec115c8da1ac236bb57de
James Christopher Adduono <jc@adduono.com>
2016-04-25 21:56:26 -0400

    Build scripts

---
+++ build.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# simple bash script for executing build
+
+# root directory of NetHunter kipper git repo (default is this script's location)
+RDIR=$(pwd)
+
+[ "$VER" ] ||
+# version number
+VER=$(cat "$RDIR/VERSION")
+
+# directory containing cross-compile arm64 toolchain
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_aarch64-linux-gnu
+
+# amount of cpu threads to use in kernel make process
+THREADS=5
+
+############## SCARY NO-TOUCHY STUFF ###############
+
+export ARCH=arm64
+export CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu-
+
+cd "$RDIR"
+
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || DEVICE=kipper
+[ "$TARGET" ] || TARGET=nethunter
+
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] || {
+	echo "Config $DEFCONFIG not found in $ARCH configs!"
+	exit 1
+}
+
+KDIR=$RDIR/build/arch/$ARCH/boot
+export LOCALVERSION="$TARGET-$DEVICE-$VER"
+
+ABORT()
+{
+	echo "Error: $*"
+	exit 1
+}
+
+CLEAN_BUILD()
+{
+	echo "Cleaning build..."
+	cd "$RDIR"
+	rm -rf build
+}
+
+SETUP_BUILD()
+{
+	echo "Creating kernel config..."
+	cd "$RDIR"
+	mkdir -p build
+	make -C "$RDIR" O=build "$DEFCONFIG" || ABORT "Failed to set up build"
+}
+
+BUILD_KERNEL()
+{
+	echo "Starting build for $LOCALVERSION..."
+	while ! make -C "$RDIR" O=build -j"$THREADS"; do
+		read -p "Build failed. Retry? " do_retry
+		case $do_retry in
+			Y|y) continue ;;
+			*) return 1 ;;
+		esac
+	done
+}
+
+BUILD_DTB()
+{
+	echo "Generating dtb.img..."
+	"$RDIR/scripts/dtbTool/dtbTool" -o "$KDIR/dtb.img" "$KDIR/" -s 2048 || ABORT "Failed to generate dtb.img!"
+}
+
+CLEAN_BUILD && SETUP_BUILD && BUILD_KERNEL && BUILD_DTB && echo "Finished building $LOCALVERSION!"
